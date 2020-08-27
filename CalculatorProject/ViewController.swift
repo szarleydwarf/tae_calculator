b4439a46e89c8757ba503e4e0e2988c69b546726
RJ <r.j.chomik@gmail.com>
2020-08-27 13:53:37 +0100

    Initial Commit

---
+++ CalculatorProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorProject
+//
+//  Created by The App Experts on 27/08/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
